ec7d873cfcacd812aa0316854d7d4514d68d97b9
Wade Wegner <wade.wegner@gmail.com>
2018-09-19 10:34:57 -0700

    Ooops

--- setup.sh
+++ setup.sh
@@ -9,8 +9,24 @@ invokeCmd() {
   eval $1
 }
 
+
+#### setup
+
 invokeCmd "yarn install"
+
+CMT_FILE="force-app/main/default/customMetadata/UntappedAPIConnection.Default.md-meta.xml"
+TARGET_KEY="yourclientid"
+sed -i '' "s/$TARGET_KEY/$UNTAPPD_CLIENT_ID/g" $CMT_FILE
+
+TARGET_KEY="yourclientsecret"
+sed -i '' "s/$TARGET_KEY/$UNTAPPD_CLIENT_SECRET/g" $CMT_FILE
+
+git update-index --assume-unchanged force-app/main/default/customMetadata/UntappedAPIConnection.Default.md-meta.xml
+
 invokeCmd "sfdx force:config:set defaultdevhubusername=gs0"
+
+#### deploy
+
 invokeCmd "sfdx force:org:create -s -f config/project-scratch-def.json --json"
 invokeCmd "sfdx force:source:push --json"
 invokeCmd "sfdx force:user:permset:assign -n UntappedIntegration --json"
